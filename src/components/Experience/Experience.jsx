6f294d7bf0c4171bde375bcfbb5a92051daf658e
anandshiva10 <anandshiva10@gmail.com>
2025-04-09 23:29:28 -0400

    changes in Experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -20,25 +20,19 @@ const Experience = () => {
       position: "right" // right side of timeline
     },
     {
-      title: "Software Engineer",
-      organization: "DataFlow Systems",
-      period: "Mar 2020 - Dec 2022",
-      details: null,
+      title: "Senior Systems Enginner",
+      organization: "Infosys",
+      period: "Sep 2021 - July 2023",
+      details:"Hyderabad, India" ,
       position: "left" // left side of timeline
     },
-    {
-      title: "Junior Developer",
-      organization: "WebSphere Inc.",
-      period: "Jul 2018 - Feb 2020",
-      details: null,
-      position: "right" // right side of timeline
-    },
+
     {
       title: "Bachelor of Technology in Computer Engineering",
-      organization: "Indian Institute of Technology",
-      period: "Aug 2012 - May 2016",
-      details: "CGPA: 3.8/4.0",
-      position: "left" // left side of timeline
+      organization: "JNTUH",
+      period: "July 2016 - Sep 2019",
+      details: "CGPA: 7.7/10.0",
+      position: "right" // left side of timeline
     }
   ];
 
