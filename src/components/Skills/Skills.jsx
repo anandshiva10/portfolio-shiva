d8ce27e7c23d925adfbbfe10f943e3a190c5401d
anandshiva10 <anandshiva10@gmail.com>
2025-04-10 20:16:04 -0400

    Final changes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -25,7 +25,7 @@ const Skills = () => {
   return (
     <section className="skills section" id="skills">
       <h2 className="section__title">Skills</h2>
-      <span className="section__subtitle">My Technical Level</span>
+      <span className="section__subtitle">Languages & Frameworks</span>
 
       <div className="skills__container container">
         {skillsData.map((skill, index) => (
